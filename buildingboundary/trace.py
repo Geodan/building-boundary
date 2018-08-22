a2cf8564b292859243c1deabb1f701a51bab2474
Chris Lucas <chris.lucas@geodan.nl>
2018-08-22 17:59:42 +0200

    Add docstring

--- buildingboundary/trace.py
+++ buildingboundary/trace.py
@@ -6,7 +6,8 @@
 
 import concave_hull
 
-from .components.segmentation import convex_fit, merge_segments
+from .components.segmentation import (convex_fit, merge_segments,
+                                      remove_small_corners)
 from .components.intersect import compute_intersections
 from .components.regularize import compute_primary_orientations, regularize_lines
 from .components.align import align_by_intercept
@@ -17,6 +18,37 @@ def trace_boundary(points, k, max_error, merge_angle, num_points=float('inf'),
                    max_intersect_distance=float('inf'), alignment=0,
                    primary_orientations=None, inflate=False):
     """
+    Trace the boundary of a set of 2D points.
+
+    Parameters
+    ----------
+    points : (Mx2) array
+        The coordinates of the points
+    k : int
+        The amount of nearest neighbors used in the concave hull algorithm
+    max_error : float
+        The maximum average error (distance) the points make with the fitted line
+    merge_angle : float
+        The angle (in radians) difference within two segments will be merged
+    num_points : int, optional
+        The number of points a segment needs to be supported by to be considered
+        a primary orientation. Will be ignored if primary orientations are set
+        manually.
+    max_intersect_distance : float, optional
+        The maximum distance an found intersection can be from the segments.
+    alignment : float, optional
+        If set segments will be aligned (their intercept be set equal by averaging)
+        if the difference between their x-axis intercepts is within this number.
+    primary_orientations : list of floats, optional
+        The desired primary orientations (in radians) of the boundary. If set manually
+        here these orientations will not be computed.
+    inflate : bool
+        If set to true the fit lines will be moved to the furthest outside point.
+
+    Returns
+    -------
+    : (Mx2) array
+        The vertices of the computed boundary line
     """
     boundary_points = concave_hull.compute(points, k, True)
 
