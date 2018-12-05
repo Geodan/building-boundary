0771213207962baf5eb6211a7fa3538f278c19e1
Chris Lucas <chris.lucas@geodan.nl>
2018-12-05 11:31:47 +0100

    improved intersect distance computation

--- buildingboundary/components/intersect.py
+++ buildingboundary/components/intersect.py
@@ -12,11 +12,28 @@ def distance(p1, p2):
     return math.hypot(*(p1-p2))
 
 
+def dist_point_to_line_segment(point, segment):
+    # http://geomalgorithms.com/a02-_lines.html
+    v = segment[1] - segment[0]
+    w = point - segment[0]
+
+    c1 = np.dot(w, v)
+    if c1 <= 0:
+        return distance(point, segment[0])
+
+    c2 = np.dot(v, v)
+    if c2 <= c1:
+        return distance(point, segment[1])
+
+    b = c1 / c2
+    pb = segment[0] + b * v
+    return distance(point, pb)
+
+
 def distance_to_intersect(segment1, segment2, intersect):
-    end_points = [segment1.end_points[0], segment1.end_points[1],
-                  segment2.end_points[0], segment2.end_points[1]]
-    distances = [distance(point, intersect) for point in end_points]
-    return min(distances)
+    distance1 = dist_point_to_line_segment(intersect, segment1.end_points)
+    distance2 = dist_point_to_line_segment(intersect, segment2.end_points)
+    return min([distance1, distance2])
 
 
 def compute_intersections(segments, max_interect_distance=float('inf')):
@@ -45,10 +62,14 @@ def compute_intersections(segments, max_interect_distance=float('inf')):
             # and intersect using the new line
             if i != num_segments - 1:
                 p = segments[i + 1].end_points[0]
-                slope = -(1 / segments[i + 1].slope) if segments[i + 1].slope != 0 else -(1 / 0.00000001)
+                slope = (-(1 / segments[i + 1].slope)
+                         if segments[i + 1].slope != 0
+                         else -(1 / 0.00000001))
             else:
                 p = segments[0].end_points[0]
-                slope = -(1 / segments[0].slope) if segments[0].slope != 0 else -(1 / 0.00000001)
+                slope = (-(1 / segments[0].slope)
+                         if segments[0].slope != 0
+                         else -(1 / 0.00000001))
 
             intercept = - slope * p[0] + p[1]
             intersect = segments[i].line_intersect([slope, intercept])
