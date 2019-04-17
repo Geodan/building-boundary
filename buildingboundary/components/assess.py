2cade75c76b7f0b529d4506e64bdf3ce1662cd93
Chris Lucas <chris.lucas@geodan.nl>
2019-04-17 17:53:36 +0200

    Only restore segments that got merged

--- buildingboundary/components/assess.py
+++ buildingboundary/components/assess.py
@@ -60,6 +60,7 @@ def restore(segments, original_segments, invalid_segments, merged_segments):
     for invalid in invalid_segments:
         restore_segments = [original_segments[i] for
                             i in merged_segments[invalid]]
-        segments[invalid+offset:invalid+offset+1] = restore_segments
-        offset += len(restore_segments)-1
+        if len(restore_segments) > 1:
+            segments[invalid+offset:invalid+offset+1] = restore_segments
+            offset += len(restore_segments)-1
     return segments
