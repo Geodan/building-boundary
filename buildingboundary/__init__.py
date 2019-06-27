d9d02c637e7207f2aed7909ac062f66e4084aee1
Chris Lucas <chris.lucas@geodan.nl>
2019-06-27 15:54:47 +0200

    Fix init file

--- buildingboundary/__init__.py
+++ buildingboundary/__init__.py
@@ -3,8 +3,19 @@ from .components import intersect
 from .components import regularize
 from .components import segment
 from .components import segmentation
-from .components import assess
 from .components import alphashape
 from .components import boundingbox
 from .components import merge
 from .utils import angle
+
+__all__ = [
+    'trace_boundary',
+    'intersect',
+    'regularize',
+    'segment',
+    'segmentation',
+    'alphashape',
+    'boundingbox',
+    'merge',
+    'angle'
+]
