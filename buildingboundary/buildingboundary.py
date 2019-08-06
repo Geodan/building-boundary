f586a6f82fba4b467c35a5736250dba030d0b5ce
Chris Lucas <chris.lucas@geodan.nl>
2019-08-06 14:39:59 +0200

    Rename

--- buildingboundary/buildingboundary.py
+++ buildingboundary/buildingboundary.py
@@ -12,13 +12,12 @@ import concave_hull
 
 from .shapes.alphashape import compute_alpha_shape
 from .shapes.boundingbox import compute_bounding_box
-from .components.segment import BoundarySegment
-from .components.segmentation import boundary_segmentation
-from .components.merge import merge_segments
-from .components.intersect import compute_intersections
-from .components.regularize import (get_primary_orientations,
-                                    regularize_segments)
-from .components.inflate import inflate_polygon
+from .core.segment import BoundarySegment
+from .core.segmentation import boundary_segmentation
+from .core.merge import merge_segments
+from .core.intersect import compute_intersections
+from .core.regularize import get_primary_orientations, regularize_segments
+from .core.inflate import inflate_polygon
 from . import utils
 
 
