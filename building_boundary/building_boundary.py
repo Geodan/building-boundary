c50fbb2085c2006d8e63b68882b33296cac8c5ec
Chris Lucas <chris.lucas@geodan.nl>
2019-08-06 15:51:41 +0200

    Rename parameters and update doc string

--- building_boundary/building_boundary.py
+++ building_boundary/building_boundary.py
@@ -21,10 +21,10 @@ from .core.inflate import inflate_polygon
 from . import utils
 
 
-def trace_boundary(points, max_error, alpha=None, k=None,
-                   num_points=None, angle_epsilon=0.05, merge_distance=None,
-                   primary_orientations=None, perp_dist_weight=3,
-                   max_error_invalid=None, inflate=False):
+def trace_boundary(points, ransac_threshold, max_error=None, alpha=None,
+                   k=None, num_points=None, angle_epsilon=0.05,
+                   merge_distance=None, primary_orientations=None,
+                   perp_dist_weight=3, inflate=False):
     """
     Trace the boundary of a set of 2D points.
 
@@ -32,34 +32,46 @@ def trace_boundary(points, max_error, alpha=None, k=None,
     ----------
     points : (Mx2) array
         The coordinates of the points.
+    ransac_threshold : float
+        Maximum distance for a data point to be classified as an inlier during
+        the RANSAC line fitting.
     max_error : float
-        The maximum error (distance) the points make with the fitted
-        line.
-
+        The maximum error (distance) a point can have to a computed line.
+    alpha : float
+        Set to determine the boundary points using an alpha shape using this
+        chosen alpha. If both alpha and k are set both methods will be used and
+        the resulting shapes merged to find the boundary points.
     k : int
-        The amount of nearest neighbors used in the concave hull algorithm
-
-    merge_angle : float
-        The angle (in radians) difference within two segments can be merged
-
+        Set to determine the boundary points using a knn based concave hull
+        algorithm using this amount of nearest neighbors. If both alpha and k
+        are set both methods will be used and the resulting shapes merged to
+        find the boundary points.
     num_points : int, optional
         The number of points a segment needs to be supported by to be
         considered a primary orientation. Will be ignored if primary
         orientations are set manually.
-
-
+    angle_epsilon : float
+        The angle (in radians) difference within two angles are considered the
+        same. Used to merge segments.
+    merge_distance : float
+        If the distance between two parallel sequential segments (based on the
+        angle epsilon) is lower than this value the segments get merged.
     primary_orientations : list of floats, optional
         The desired primary orientations (in radians) of the boundary. If set
         manually here these orientations will not be computed.
-
+    perp_dist_weight : float
+        Used during the computation of the intersections between the segments.
+        If the distance between the intersection of two segments and the
+        segments is more than `perp_dist_weight` times the distance between the
+        intersection of the perpendicular line at the end of the line segment
+        and the segments, the perpendicular intersection will be used instead.
     inflate : bool
         If set to true the fit lines will be moved to the furthest outside
         point.
 
-
     Returns
     -------
-    : (Mx2) array
+    vertices : (Mx2) array
         The vertices of the computed boundary line
     """
     if alpha is not None:
@@ -82,14 +94,14 @@ def trace_boundary(points, max_error, alpha=None, k=None,
 
     bounding_box = compute_bounding_box(boundary_points,
                                         given_angles=primary_orientations,
-                                        max_error=max_error_invalid)
+                                        max_error=max_error)
 
     distances = [bounding_box.exterior.distance(Point(p)) for
                  p in boundary_points]
-    if max(distances) < max_error_invalid:
+    if max(distances) < max_error:
         return np.array(bounding_box.exterior.coords)
 
-    segments = boundary_segmentation(boundary_points, max_error)
+    segments = boundary_segmentation(boundary_points, ransac_threshold)
 
     if len(segments) in [0, 1, 2]:
         return np.array(bounding_box.exterior.coords)
@@ -107,12 +119,12 @@ def trace_boundary(points, max_error, alpha=None, k=None,
 
     boundary_segments = regularize_segments(boundary_segments,
                                             primary_orientations,
-                                            max_error=max_error_invalid)
+                                            max_error=max_error)
 
     boundary_segments = merge_segments(boundary_segments,
                                        angle_epsilon=angle_epsilon,
                                        max_distance=merge_distance,
-                                       max_error=max_error_invalid)
+                                       max_error=max_error)
 
     vertices = compute_intersections(boundary_segments,
                                      perp_dist_weight=perp_dist_weight)
